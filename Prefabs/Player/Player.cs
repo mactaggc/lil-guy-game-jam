d12ece9cadf96d60884ad6117809d2301ccb90c3
mactaggc <mactaggc@oregonstate.edu>
2025-10-06 19:47:26 -0700

    Made some adjustments to player movement and added a tilemap for the game environment.

--- Prefabs/Player/Player.cs
+++ Prefabs/Player/Player.cs
@@ -3,10 +3,18 @@ using System;
 
 public partial class Player : CharacterBody2D
 {
-    public const int SPEED = 300;
-    public const int JUMP_VELOCITY = -400;
+    [Export] public float SPEED = 300;
+    [Export] public float JUMP_VELOCITY = -400;
+    pMovement movement;
     public override void _Ready()
     {
         AddToGroup("player");
+        movement = GetNode<pMovement>("Movement");
     }
+
+    public override void _Process(double delta)
+    {
+        movement.Move((float)delta);
+    }
+
 }
--- Prefabs/Player/pMovement.cs
+++ Prefabs/Player/pMovement.cs
@@ -3,9 +3,37 @@ using System;
 
 public partial class pMovement : Node
 {
-    CharacterBody2D player;
+    Player player;
     public override void _Ready()
     {
-        player = GetNode<CharacterBody2D>("../Player");
+        player = GetParent() as Player;
+    }
+
+    public void Move(float delta)
+    {
+        Vector2 velocity = player.Velocity;
+
+        if (Input.IsActionPressed("ui_right"))
+        {
+            velocity.X = player.SPEED; // 300
+        }
+        else if (Input.IsActionPressed("ui_left"))
+        {
+            velocity.X = -player.SPEED;
+        }
+        else
+        {
+            velocity.X = 0;
+        }
+
+        if (Input.IsActionJustPressed("ui_up") && player.IsOnFloor())
+        {
+            velocity.Y = -player.JUMP_VELOCITY; // -400
+        }
+
+        velocity.Y = Math.Clamp(velocity.Y, -player.JUMP_VELOCITY, 1200);
+        velocity.Y += 1200 * delta;
+        player.Velocity = velocity;
+        player.MoveAndSlide();
     }
 }
