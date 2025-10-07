cd020ac009426e8f9f13735b78db6b56bcc70b14
mactaggc <mactaggc@oregonstate.edu>
2025-10-06 19:15:03 -0700

    Made player scene and added player sprite to main scene

--- Main.cs
+++ Main.cs
@@ -3,4 +3,15 @@ using System;
 
 public partial class Main : Node2D
 {
+    CharacterBody2D player;
+
+    public override void _Ready()
+    {
+        player = GetNode<CharacterBody2D>("Player");
+    }
+    public override void _Process(double delta)
+    {
+
+    }
+
 }
